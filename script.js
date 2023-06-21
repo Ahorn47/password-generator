8527b787a26ce91353a22c8eba1f2da94bc127aa
Ahorn47 <131202726+Ahorn47@users.noreply.github.com>
2023-06-20 20:39:38 -0500

    updated read me and used arrays/strings to build on the options for generating code/password true or false

--- script.js
+++ script.js
@@ -50,7 +50,86 @@ function generatePassword () {
         confirmSpecialCase = confirm ("Will this have special case characters?");
         console.log ("Special Characters" + confirmSpecialCase); 
     };
-    else if (confirmLower && confirmUpper confirmSpecialCase){
-        userChoices = lowercase.concat(upperCase, numbers , special);
-        console.log(userChoices);
-    }
+    // if there is nothing selected add alert so they must choose one
+        if (!confirmLower && !confirmUpper && !confirmNumber && !confirmSpecialCase){
+            userChoices= alert("You must choose 4 of the options");
+            
+// 4 options true
+        } else if (confirmLower && confirmUpper && confirmNumber && confirmSpecialCase){
+            userChoices = lowercase.concat(upperCase, numbers , special);
+            console.log(userChoices);
+        }
+
+
+        // 3 options true
+        else if(confirmLower && confirmUpper && confirmNumber) {
+            userChoices = lowercase.concat(upperCase, numbers);
+            console.log(userChoices);
+        }
+        else if (confirmLower && confirmNumber && confirmSpecialCase){
+            userChoices = lowercase.concat(upperCase, special);
+            console.log(userChoices);
+        }
+        else if (confirmUpper && confirmNumber && confirmSpecialCase){
+            userChoices = upperCase.concat(numbers, special);
+            console.log(userChoices);
+        }
+        // 2 options true
+        else if (confirmLower && confirmUpper){
+            userChoices = lowercase.concat (upperCase);
+            console.log(userChoices);
+        }
+        else if (confirmLower && confirmNumber) {
+            userChoices = lowercase.concat(numbers);
+            console.log(userChoices);
+        }
+        else if (confirmLower && confirmSpecialCase){
+            userChoices = lowercase.concat(special);
+            console.log(userChoices);
+        }
+        else if (confirmUpper && confirmNumber) {
+            userChoices = upperCase.concat(numbers);
+            console.log(userChoices);
+        }
+        else if (confirmUpper && confirmSpecialCase) {
+            userChoices = upperCase.concat(special);
+            console.log(userChoices)
+        }
+        else if (confirmNumber && confirmSpecialCase) {
+            userChoices = numbers.concat(special);
+            console.log(userChoices);
+        }
+        // 1 option true
+        else if (confirmLower) {
+            userChoices= lowercase;
+            console.log(userChoices);
+        }
+        else if (confirmUpper) {
+            userChoices= blankUpper.concat(upperCase);
+            console.log(userChoices);
+        }
+        else if (confirmNumber) {
+            userChoices = numbers;
+            console.log(userChoices);
+        }
+        else if(confirmSpecialCase) {
+            userChoices = special;
+            console.log(userChoices);
+        };
+        
+        var passwordBlank = [];
+
+        for (var i = 0; i < passwordLength; i++) {
+        var allChoices = userChoices[Math.floor(Math.random() * userChoices.length)];
+        passwordBlank.push(allChoices);
+        console.log(allChoices);
+    } 
+        var password = passwordBlank.join("");
+        console.log("Drum Roll and your password is:" + password);
+         return password;
+
+}
+
+
+
+
